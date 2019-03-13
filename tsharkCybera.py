efe8920a812dc27e685b20a452875cbdfe0be7f6
sepehrKazemian <sepehr.kazemian@gmail.com>
2019-03-12 23:26:10 -0600

    multi-device analyzer and backup implemented

--- tsharkCybera.py
+++ tsharkCybera.py
@@ -9,27 +9,37 @@ import gzip
 class tshark():
 	def __init__(self):
 		self.totalLength = 0
-		with open("node1.txt", "r") as txt:
-			node1Content = txt.readline()
-		with open("node2.txt", "r") as txt:
-			node2Content = txt.readline()
-		if node1Content == "":
-			node1Content = "1"
-		if node2Content == "":
-			node2Content = "1"
-
-		self.nodeCounter = [node1Content[0:len(node1Content)-1], node2Content[0:len(node2Content)-1]]
+		self.numbNodes = sys(arv[1])
+		self.nodeCounter = [0 for i in range(self.numbNodes)]
+		
+		#based on number of nodes that we have, we create or check the previous values of read data
+		for i in range(1, self.numbNodes + 1):
+			nodeCounterName = "node" + i + ".txt"
+			if os.path.isfile(nodeCounterName) == True:
+				if (os.stat(nodeCounterName).st_size == 0) == False:
+					with open(nodeCounterName, "r") as txt:
+						self.nodeCounter[i - 1] = str(int(txt.readline()))
+				else:
+					self.nodeCounter[i - 1] = str(0)
+			else:
+				os.system("touch node1.txt")
+				self.nodeCounter[i - 1] = str(0)	
+			
+			
 		print(self.nodeCounter)
 		self.secondChar = None
 
 	def continuousReading(self):
-		nodeName1 = 'node1'
-		nodeName2 = 'node2'
-		thread1 = threading.Thread(target = self.runningCommand, args = (nodeName1, 0))
-		thread2 = threading.Thread(target = self.runningCommand, args = (nodeName2, 1))
-		thread1.start()
-		thread2.start()
-
+		#we are creating pool of threads for each node
+		threadPool = []
+		for i in range(self.numbNodes):
+			nodeName = "node" + str(i + 1)
+			thread = threading.Thread(target = self.runningCommand, args = (nodeName, i))
+			threadPool.append(thread)
+		
+		for i in range(self.numbNodes):
+			threadPool[i].start()
+		
 
 	def runningCommand(self, nodeName, arrIndex):
 		fileName = nodeName + "." + str(self.nodeCounter[arrIndex])
@@ -37,26 +47,22 @@ class tshark():
 		nodeText = str(nodeName) + ".txt"
 		while True:
 			fileName = nodeName + "." + str(self.nodeCounter[arrIndex])
+			
+			#we should update the text to state for which number we are waiting
 			with open(nodeText, "w") as textStat:
 				textStat.write(str(self.nodeCounter[arrIndex]))
 			if os.path.isfile(fileName) == True:
 				print(fileName + " file is here")
-#			if os.path.isfile(fileName) == False:
-#				if startingName == 'node1.' + str(secondCounter):
-#					secondCounter += 1 
-#				startingName = 'node1.' + str(secondCounter)
-#				num = 0
-#				fileName = startingName + str(num)
+				#when we got the number we should pass it to tshark to analyze it
 				tsharkCommand = 'tshark -r ' + fileName + ' -Tfields -e wlan.ta -e wlan_radio.channel -e wlan.qbss.cu -e wlan_radio.signal_dbm -e frame.time -E separator=/s'
-			#should call pipe for the subprocess to read and write simultaneously
+				#should call pipe for the subprocess to read and write simultaneously
 				proc = Popen(tsharkCommand, stdout = PIPE, shell = True)
 	
 				while proc.poll() is None:
 					time.sleep(0.00001)
 					#for every incoming line we should block and then read it
 					line = proc.stdout.readline()
-#					print(line.decode('ascii'))
-#					print(line)
+					#sending line for analyzing
 					self.analyze(line.decode('ascii'), arrIndex)
 			
 				with open(fileName, 'rb') as inFile:
@@ -65,6 +71,8 @@ class tshark():
 				with gzip.GzipFile(filename= gzName, mode="wb", compresslevel=9) as file:
 					file.write(content)
 				print(gzName + " is completed")
+				
+				#uploading files into gdrive as backup/ up to 4 device is supported in this code
 				if arrIndex == 0:
 					os.system("/home/ubuntu/gdrive-linux-x64 upload --parent 1KyFUjKDHIR5hapu02re-b7z5cf2mBEDf " + gzName )
 					print(gzName + " is uploaded")
@@ -72,23 +80,20 @@ class tshark():
 				elif arrIndex == 1:
 					os.system("/home/ubuntu/gdrive-linux-x64 upload --parent 101ltxcgr5dyChf5JEG913pds-uzSZ1sK " + gzName )
 					print(gzName + " is uploaded")
+					
+				elif arrIndex == 2:
+					os.system("/home/ubuntu/gdrive-linux-x64 upload --parent 101ltxcgr5dyChf5JEG913pds-uzSZ1sK " + gzName )
+					print(gzName + " is uploaded")
+
+				elif arrIndex == 3:
+					os.system("/home/ubuntu/gdrive-linux-x64 upload --parent 101ltxcgr5dyChf5JEG913pds-uzSZ1sK " + gzName )
+					print(gzName + " is uploaded")
 
 				os.system("rm " + fileName)
 				os.system("rm " + gzName)
-#				if int(self.nodeCounter[arrIndex]) =< 100000:
 				self.nodeCounter[arrIndex] = str(int(self.nodeCounter[arrIndex]) + 1)
-#				nodeText = str(nodeName) + "txt"	
-#				with open(nodeText, "w") as textStat:
-#					textStat.write(self.nodeCounter[arrIndex])
-	#			elif int(self.nodeCounter[arrIndex]) > 100000:
-	#				if self.secondChar == None:
-	#					self.secondChar = 0
-	#					nodeName += str(self.secondChar)
-	#				else:
-	#					nodeName = nodeName[0: len(nodeName) - len(str(self.secondChar))]
-	#					self.secondChar += 1
-	#					nodeName += str(self.secondChar)
-	#				self.nodeCounter[arrIndex] = '0'
+			
+
 					
 					
 		
@@ -129,9 +134,13 @@ class tshark():
 #		print(channel)
 #		print(cu)
 		if len(macAdd) == 12:
-			directory = "node" + str(arrIndex+1)  + "/" + str(macAdd[0:12])
+			directory = "node" + str(arrIndex+1)  + "/" + str(macAdd[0:12]) + ".txt"
 			with open(directory, "a") as file:
 				file.write(time + " " + channel + " " + cu + " " + sig + "\n")
+			
+			chanDirectory = "channel/" + "channel" + str(channel) + ".txt"
+			with open(chanDirectory, "a") as file:
+				file.write(time + " " + channel + " " + cu + " " + sig + "\n")			
 		
 		else:
 			print("macAdd is: " + str(macAdd))
@@ -140,5 +149,8 @@ class tshark():
 
 if __name__ == '__main__':
 #	print("aaa")
-	tshObj = tshark()
-	tshObj.continuousReading()
+	if len(sys.argv) == 2:
+		tshObj = tshark()
+		tshObj.continuousReading()
+	else:
+		print("Enter Number of Nodes")
