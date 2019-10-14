0758b80df467038d3ab9d3bff2cbe17acb7782c6
SepehrKazemian <skazemia@ualberta.ca>
2019-10-13 19:00:25 -0600

    fixing bugs

--- costAssociateLumping.py
+++ costAssociateLumping.py
@@ -45,56 +45,14 @@ from importlib import reload
 from scipy.stats import rayleigh
 import dataManipulation as dataMan
 import matplotlib.pyplot as plt
+import processData as processData
 warnings.filterwarnings('always')
 # <codecell>
 
 #*******************************************************************************
 # %%codecell
-def processingData():
-    address = input("the address of the collected data files (not alligned files or CSV files): ")
-    fileNameArr = os.listdir(address)
 
-    extraArr = []
-    for i in range(len(fileNameArr)):
-        if ".txt" not in fileNameArr[i]:
-            extraArr.append(fileNameArr[i])
-
-    for i in range(len(extraArr)):
-        fileNameArr.remove(extraArr[i])
-
-    print(fileNameArr)
-
-
-
-    timerInMinute = 30 #chunking files to 30 minutes
-    timeInterval = 6 #seconds
-
-    CU_FileChunks = None
-    fileNameArr = ["500f80271400.txt"] #for test we are giving a ready file
-
-    LA = classImportLA.learningAlgs() #calling an object of the class
-
-    #*************checking if we have the processed version of file in our CSV or not, if not we are gonna process data otherwise just we gonna read it
-    pathFile = address + "/CSV/" + str(fileNameArr[0]) + ".csv"
-    filePath = address + "/" + str(fileNameArr[0])
-    importlib.reload(dataMan) #reload the class if it has cache (.pyc) to load the class from scratch
-    if os.path.isfile(pathFile) == False:
-            print("we do not have processed data for file " + str(fileNameArr[0]) + " so we are making it")
-            numOfLines = int(os.popen('wc -l < ' + str(filePath)).read().split()[0])
-            print("number of lines is: " + str(numOfLines))
-            dataMan.normalDataSplitting(fileNameArr[0], 0, 0, timeInterval, address)
-
-    # print(fileNameArr[0])
-    # print(address)
-    stat, data = LA.csvChecker(fileNameArr[0], 0, address)
-
-    print(data.head())
-    print("now we have the processed data from pandas")
-    data["CU/255"] = data["CU"] / 255 #we add a column to our pandas table
-
-    return data
-
-dataFrame = processingData()
+dataFrame = processData.processingData()
 #address is: /home/netlab/Desktop/thesis/data/1node1-3-5/
 #/home/netlab/Desktop/thesis/data/500f80271400/
 data = dataFrame.copy() #copying the dataFrame to have a copy of not edited data
@@ -104,78 +62,14 @@ print("hello")
 
 #*******************************************************************************
 # %% codecell
-def dataFrameManipulation(data):
-    minutes = int(input("please enter how long would be the chunk minutes? "))
-    data["time"] = data["time"].apply(lambda x: x + timedelta(hours = -7)) #changing time from UTC to Mountain
 
-    #making data ready for assigning an Index to each 30 minutes interval
-    secondsPerChunk = int(60 * minutes) #for 30 minutes interval we have these many seconds
-    data["timeIndex"] = -1
-    startIndex = 0
-    timeIndexVal = 1
-    data["timeIndex"] = data["time"].apply(lambda x: math.floor(((x.second + (x.minute * 60) + (x.hour * 3600)) / secondsPerChunk)))
-    numberOfChunks = max(data["timeIndex"]) + 1
-    dataSet = data.copy() #copying the dataFrame before manipulating it again
-    print("removing weekends from the data")
-    data["weekDay"] = data["time"].apply(lambda x: x.weekday()) #printing the weekdays numbers
-    data = data.drop(data["weekDay"].loc[(data["weekDay"] == 6) | ((data["weekDay"] == 5))].index, axis=0)
-    return data
-
-data = dataFrameManipulation(data)
-
-def markovianTransitionMatrixDegree1(data, coeffBool):
-    dataCpy = data.copy()
-#     dataCpy = data
-    numberOfChunks = max(data["timeIndex"]) + 1
-    cuTrans = np.zeros(shape=(numberOfChunks, 255, 255))
-    start = -1
-    next = -1
-    prevChunkVal = -1
-    newChunkVal = -1
-    firstIndexOfChunk = -1
-
-
-
-#     cuTrans = np.zeros(shape=(numberOfChunks, 26, 26))
-#     cuDifTrans = np.zeros(shape=(numberOfChunks, 26, 26))
-
-    start = -1
-    next = -1
-    prevChunkVal = -1
-    newChunkVal = -1
-    firstIndexOfChunk = -1
-#     indexesOfChunks = dataCpy.index[dataCpy["timeIndex"] == i]
-    counter = 0
-    maxValuePlusOne = 27
-
-    for x in range(numberOfChunks):
-        iterPandas = data.loc[data["timeIndex"] == x]
-        start = -1
-        next = -1
-
-        for index, row in iterPandas.iterrows():
-            start = next
-            next = row["CU"]
-            if start != -1:
-                cuTrans[x, math.floor(start), math.floor(next)] += 1
-    return cuTrans
-
-def normalizingTransMatrix(cuTrans):
-    number_of_samples = np.zeros(shape=(cuTrans.shape[0], cuTrans.shape[1]))
-    ans = cuTrans.copy()
-    for x in range(cuTrans.shape[0]):
-        for i in range(cuTrans.shape[1]):
-            sum = 0
-            sum = np.sum(ans[x][i])
-            number_of_samples[x][i] = int(sum)
-            if sum != 0:
-                ans[x][i] = ans[x][i]/sum
-    return ans, number_of_samples
-
+data = processData.dataFrameManipulation(data)
+numberOfStates = 255
 ##For 30 minutes chunk
-cuTrans = markovianTransitionMatrixDegree1(data, 0)
-normalizedCuTrans, number_of_samples = normalizingTransMatrix(cuTrans)
+cuTrans = processData.markovianTransitionMatrixDegree1(data, numberOfStates)
+normalizedCuTrans = processData.normalizingTransMatrix(cuTrans)
 cuTrans_cpy = normalizedCuTrans.copy()
+print(cuTrans_cpy[0])
 # <codecell>
 #*******************************************************************************
 
@@ -262,11 +156,19 @@ def mergingStates(normalizedCuTrans, min_diff):
     extra_Normalized_Transition_Matrix = np.zeros(shape = (normalizedCuTrans.shape[1] - 1,
                                                            normalizedCuTrans.shape[1] - 1))
 
+    # print("sum of the min_diff: ", np.sum(normalizedCuTrans[min_diff]))
+    # print("sum of the min_diff + 1: ", np.sum(normalizedCuTrans[min_diff + 1]))
+    if np.sum(normalizedCuTrans[min_diff, :]) != 0 and np.sum(
+        normalizedCuTrans[min_diff + 1, :]) != 0:
+        normalizedCuTrans[min_diff, :] = normalizedCuTrans[min_diff, :] + normalizedCuTrans[min_diff + 1, :]
+        normalizedCuTrans[min_diff, :] = normalizedCuTrans[min_diff, :] /2
+    else:
+        normalizedCuTrans[min_diff, :] = normalizedCuTrans[min_diff, :] + normalizedCuTrans[min_diff + 1, :]
+
     normalizedCuTrans[:, min_diff] = normalizedCuTrans[:, min_diff] + normalizedCuTrans[:, min_diff + 1]
-    normalizedCuTrans[min_diff, :] = normalizedCuTrans[min_diff, :] + normalizedCuTrans[min_diff + 1, :]
-    normalizedCuTrans[min_diff, :] = normalizedCuTrans[min_diff, :] /2
     extraNumpy = np.delete(normalizedCuTrans, min_diff + 1, 0)
     extra_Normalized_Transition_Matrix = np.delete(extraNumpy, min_diff + 1, 1)
+    # print("sum of this row now is: ", np.sum(extra_Normalized_Transition_Matrix[min_diff]))
 
     return extra_Normalized_Transition_Matrix
 
@@ -282,7 +184,11 @@ def updatePercentage(percentageMatrix, min_diff):
 
 def clustering(normalizedTransitionMatrix):
 
+    overal_merged_states = []
+    overal_bandwidth_vector = []
+
     for i in range(normalizedTransitionMatrix.shape[0]):
+    # for i in range(1):
 
         percentageMatrix = bandwidthPercentage(normalizedTransitionMatrix[i])
         # a percentage Matrix for each time interval
@@ -320,7 +226,6 @@ def clustering(normalizedTransitionMatrix):
                 mergedStates = mergingStates(mergedStates, min_index_in_penalty)
                 # merging states with closest difference w.r.t penalty
 
-
                 percentageMatrix = updatePercentage(percentageMatrix, min_index_in_penalty)
                 # updating bandwidth percentage after merging states
 
@@ -331,44 +236,14 @@ def clustering(normalizedTransitionMatrix):
                 print("the value of i is: ", i)
                 print(mergedStates)
                 print(percentageMatrix)
+                overal_merged_states.append(mergedStates)
+                overal_bandwidth_vector.append(percentageMatrix)
                 break
+
+    return overal_merged_states, overal_bandwidth_vector
 
 
 
 
 transExtra = cuTrans_cpy.copy()
-clustering(transExtra)
-
-min_diff = np.argmin(neighbor_difference_array) #minimum of difference is for x and x + 1
-
-cuTransCopy = cuTrans.copy()
-
-
-cuTransCopy[20][:, min_diff] = cuTransCopy[20][:, min_diff] + cuTransCopy[20][:, min_diff - 1]
-cuTransCopy[20][min_diff, :] = cuTransCopy[20][min_diff, : ] + cuTransCopy[20][min_diff - 1, : ]
-
-print(cuTrans[20][2])
-print(cuTrans[20][3])
-print(np.sum(cuTrans[20][2]))
-print(np.sum(cuTrans[20][3]))
-
-print(neighbor_difference_array)
-
-# neighbor_difference_list = [0 for i in range(statePenaltyNumpy.shape[1])]
-#
-# for i in range(statePenaltyNumpy.shape[1]):
-#     if i == 0:
-#         neighbor_difference_list[i] = (np.inf, np.abs(statePenaltyNumpy[20][i] - statePenaltyNumpy[20][i + 1]))
-#
-#     elif i == (statePenaltyNumpy.shape[1] - 1):
-#         neighbor_difference_list[i] = (np.abs(statePenaltyNumpy[20][i] -
-#                                             statePenaltyNumpy[20][i - 1]), np.inf)
-#
-#     else:
-#         neighbor_difference_list[i] = (np.abs(statePenaltyNumpy[20][i] -
-#                                             statePenaltyNumpy[20][i - 1])
-#                                     , np.abs(statePenaltyNumpy[20][i] -
-#                                           statePenaltyNumpy[20][i + 1]))
-#
-# neighbor_difference_list
-print(statePenaltyNumpy[20])
+overal_merged_states, overal_bandwidth_vector = clustering(transExtra)
